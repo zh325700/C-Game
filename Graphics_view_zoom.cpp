c9d632f1c4327bf57e971bd7ded8e44ad3912aba
Han Zhang <han,zhang@student.kuleuven.be>
2017-12-07 14:25:41 +0100

    add auto zoom in and zoom out, with parameter graphicView

---
+++ Graphics_view_zoom.cpp
@@ -0,0 +1,6 @@
+#include "Graphics_view_zoom.h"
+
+Graphics_view_zoom::Graphics_view_zoom()
+{
+
+}
---
+++ Graphics_view_zoom.h
@@ -0,0 +1,11 @@
+#ifndef GRAPHICS_VIEW_ZOOM_H
+#define GRAPHICS_VIEW_ZOOM_H
+
+
+class Graphics_view_zoom
+{
+public:
+    Graphics_view_zoom();
+};
+
+#endif // GRAPHICS_VIEW_ZOOM_H
